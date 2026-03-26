7350320c4a3edea93815820087363a162dbc2666
c1vd <litvinoffarteom999@gmail.com>
2026-03-26 20:55:07 +0700

    Security update

--- src/main/java/com/company/mysn/security/SecurityConfig.java
+++ src/main/java/com/company/mysn/security/SecurityConfig.java
@@ -3,15 +3,24 @@ package com.company.mysn.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.company.mysn.services.MyUserDetailsService;
+
 
 @Configuration
 public class SecurityConfig {
+    @Bean
+    UserDetailsService userDetailsService(){
+        return new MyUserDetailsService();
+    }
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/admin").denyAll()
                 .anyRequest().permitAll()   // Allow all requests without authentication
             );
 
---
+++ src/main/java/com/company/mysn/services/MyUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.company.mysn.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.company.mysn.repositories.UserRepository;
+
+public class MyUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var user = userRepository.findUserByNickname(username).orElseThrow();
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getNickname())
+                .password(user.getPasswordHash())
+                .build();
+    }
+}
